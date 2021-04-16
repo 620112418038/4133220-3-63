146dc00c91e45dc1822a27a255d95df124b317c1
620112418038 <ongunya090343@gmail.com>
2021-04-16 15:56:08 +0700

    LAB6

---
+++ LAB5/conDB.php
@@ -0,0 +1,18 @@
+<?php
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(E_ALL);
+
+$host = "sql305.byethost6.com";
+$user = "b6_28399659";
+$pass = "x1a8@1f";
+$db = "b6_28399659_member";
+
+try{
+    $conn = new mysqli($host,$user,$pass,$db);
+    //echo "Cnnected !!"
+}
+catch (Exception $e){
+    echo $e->getMessage();
+}
+?>
